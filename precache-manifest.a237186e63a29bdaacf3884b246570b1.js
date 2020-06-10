d1c08046f7f877fb1f0656eed0784b1a4fc77791
5377 <pradipamate07@gmail.com>
2020-06-10 20:57:38 +0530

    Updates

---
+++ precache-manifest.a237186e63a29bdaacf3884b246570b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4130c8900279fab2c756f2d7f5f59006",
+    "url": "/reduxtodolist/index.html"
+  },
+  {
+    "revision": "9b896c249638e3b87772",
+    "url": "/reduxtodolist/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "bc4a1b31ac9616ad2446",
+    "url": "/reduxtodolist/static/css/main.ea954872.chunk.css"
+  },
+  {
+    "revision": "9b896c249638e3b87772",
+    "url": "/reduxtodolist/static/js/2.7a812945.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reduxtodolist/static/js/2.7a812945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4a1b31ac9616ad2446",
+    "url": "/reduxtodolist/static/js/main.8f37d05e.chunk.js"
+  },
+  {
+    "revision": "0824284a05567c2d8b22",
+    "url": "/reduxtodolist/static/js/runtime-main.4d5f7b91.js"
+  }
+]);
