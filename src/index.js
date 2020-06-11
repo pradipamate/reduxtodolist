6b90ea801c21b87c4b133bd09f87493405a0f1d7
5377 <pradipamate07@gmail.com>
2020-06-11 18:01:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import store from './store/store'
+import store from './store/store';
+
 
 const jsx=(
    <Provider store={store}>
--- src/store/store.js
+++ src/store/store.js
@@ -3,18 +3,6 @@ import {v1 as uuid} from "uuid";
 import{composeWithDevTools} from 'redux-devtools-extension';
 import Rootreducer from "../reducer/Rootreducer"
 
-// const initialstage = {
-//     todos:[
-//          {
-//          id:uuid(),
-//          name:"go to gym"
-//          },
-//          {
-//             id:uuid(),
-//             name:"go to gym2"
-//          }
-//       ]
-// }
 
 const store = createStore(Rootreducer,composeWithDevTools())
 export default store;
